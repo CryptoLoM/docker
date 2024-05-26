737201a21b9c8aef45371b9d248ff3b0756dd948
CryptoLoM <153677367+CryptoLoM@users.noreply.github.com>
2024-05-27 02:31:50 +0300

    Delete Docker directory

--- Docker/main.py
+++
@@ -1,8 +0,0 @@
-from math import pi
-
-def circle_area(radius):
-    if radius<0:
-        raise ValueError('Impossible value')
-    if type(radius) not in [int, float]:
-        raise TypeError('Impossible types')
-    return pi*radius**2
